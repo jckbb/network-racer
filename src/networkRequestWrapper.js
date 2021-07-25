0c9dce3a39a8855efed203fee5140dc438d4722c
jckbb <josephkbburghard@gmail.com>
2021-07-25 15:38:37 -0700

    Refactor filename to better respresent the content

--- src/networkRequestWrapper.js
+++ src/networkRequestWrapper.js
@@ -1,11 +1,13 @@
-
+// thrown error if timeout occurs
 export const TIMEOUT_ERROR = 'Timeout';
 
+// default time(ms)
 const DEFAULT_DURATION = 10000;
 
 // race timeout
-export default networkWrapper = async (request, duration = DEFAULT_DURATION) => {
+export default async (request, duration = DEFAULT_DURATION) => {
   try {
+    // run both request and setTimeout(), which ever finishes first is the winner
     return await Promise.race([
       request,
       new Promise((_, reject) =>
